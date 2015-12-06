045aadb7db9c592f0333b887d540a9771fa6eb27
Aaron Redepenning <aaronredepenning@live.com>
2015-12-06 12:14:51 -0600

    Mobile sidebar support

--- client/app/common/directives/topbar/topbar.js
+++ client/app/common/directives/topbar/topbar.js
@@ -3,6 +3,9 @@ angular.module('dashboard.topbar', [])
 .directive('dashboardTopbar', function() {
   return {
     restrict: 'E',
+    scope: {
+      collapsed: '='
+    },
     templateUrl: 'common/directives/topbar/topbar.html'
   }
 });
