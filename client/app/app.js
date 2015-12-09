2850c7bfb63a79f9c1a465ac165f137fd354dbfe
Aaron Redepenning <aaronredepenning@live.com>
2015-12-09 12:23:16 -0600

    Use angular . on xbee disconnect

--- client/app/app.js
+++ client/app/app.js
@@ -41,7 +41,7 @@ angular.module('dashboard', [
     $scope.averageTemp = averageTemp();
     $scope.averageHum = averageHum();
 
-    $scope.$broadcast('xbeeDisconnected', {id: message});
+    $rootScope.$broadcast('xbeeDisconnected', {id: message});
   });
 
   $socket.on('update', function(message) {
