2bc00d741f1f1dfdc21438697e33d7dc706cb426
Aaron Redepenning <aaronredepenning@live.com>
2015-12-09 11:20:43 -0600

    Added

--- server/server.js
+++ server/server.js
@@ -69,10 +69,10 @@ io.on('connection', function(socket){
 	if (xbee_set === false) {
 			xbeeAPI.on("frame_object", function(frame) {
 				//Recieved response from XBee, cancel disconnect timeout
-				// if(xbee_set) {
-				// 	console.log('Clearing Timeout for: ' + frame.remote16);
-				// 	clearTimeout(XBeeTimeouts[frame.remote16]);
-				// }
+				if(XBeeTimeouts.hasOwnProperty(frame.remote16)) {
+					console.log('Clearing Timeout for: ' + frame.remote16);
+					clearTimeout(XBeeTimeouts[frame.remote16]);
+				}
         //console.log(frame);
         var data = {
           type: frame.type,
@@ -84,10 +84,10 @@ io.on('connection', function(socket){
         };
 				io.emit('update', data);
 				//Set a 5 second timeout to wait for next XBee response
-				XBeeTimeouts = setTimeout(function() {
+				XBeeTimeouts[frame.remote16] = setTimeout(function() {
 					console.log('disconnect');
 				}, 10000);
-				clearTimeout(XBeeTimeouts);
+				console.log(XBeeTimeouts);
 			});
 			xbee_set = true;
 		}
